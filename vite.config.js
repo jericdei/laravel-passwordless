c8a59913ce06fba7f8b16f76f636a505c9f11e2d
Jeric June Logan <jericjune.logan.lhoopa@gmail.com>
2024-05-29 15:52:28 +0800

    fix ziggy

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
 import vue from "@vitejs/plugin-vue";
+import path from "path";
 
 export default defineConfig({
     plugins: [
@@ -10,6 +11,11 @@ export default defineConfig({
         }),
         vue(),
     ],
+    resolve: {
+        alias: {
+            "ziggy-js": path.resolve("vendor/tightenco/ziggy"),
+        },
+    },
     server: {
         host: "0.0.0.0",
         hmr: {
